2357d92ace7f2547c27b223ebb781fc4a31f178c
cristianA123 <cristianchipan2@gmail.com>
2022-11-27 22:20:26 -0500

    agregar modulo categoria

---
+++ src/modules/platform/modules/category/router/index.js
@@ -0,0 +1,26 @@
+export default {
+    name: 'category',
+    component: () => import('../layouts/CategoryLayout.vue'),
+    children: [
+        
+        {
+            path: 'lista-categorias',
+            name: 'list-categories',
+            component: () => import('../views/ListCategory.vue')
+        },
+        {
+            path: 'nueva-categoria',
+            name: 'new-category',
+            component: () => import('../views/NewCategory.vue')
+        },
+        {
+            path: 'editar-categoria/:id',
+            name: 'update-category',
+            component: () => import('../views/UpdateProduct.vue'),
+            props: (route) => { 
+              const id = Number( route.params.id )
+              return isNaN( id ) ? { id: 1} : { id } 
+            }
+          }
+    ]    
+}
--- src/modules/platform/router/index.js
+++ src/modules/platform/router/index.js
@@ -1,5 +1,6 @@
 import isAuthenticatedGuard from '../../auth/router/auth-guard.js'
 import productRouter from '../modules/product/router/index.js'
+import categorytRouter from '../modules/category/router/index.js'
 import workerRouter from '../modules/worker/router/index.js'
 export default {
     name: 'platform',
@@ -12,6 +13,10 @@ export default {
         {
             path :'/platform/product/',
             ...productRouter
+        },
+        {
+            path :'/platform/category/',
+            ...categorytRouter
         },
         {
             path :'/platform/worker/',
