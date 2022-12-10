467408bedbb58b3b250d736484b0e3db391618f8
YoshiBot <surke@outlook.es>
2022-12-10 00:25:23 -0500

    buidl previo al desplegue

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/Farmacia_App_frontEnd/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
